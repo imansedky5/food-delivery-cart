ee2b2995db2832988d49a2e7fa93fa7663e253c8
imansedky5 <imans.ewaiti@gmail.com>
2022-12-30 14:45:19 +0200

    Create images folder

---
+++ src/assests/data.tsx
@@ -0,0 +1,190 @@
+export let fastFoodData = [
+    {
+      id: 1,
+      type: "pizza",
+      title: "Pepperoni Pizza",
+      url: "./images/pizza_01.jpg",
+      price: "$40",
+      description:
+        "Extra-virgin olive oil, garlic, mozzarella, mushrooms and olives.",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 2,
+      type: "pizza",
+      title: "Trio Cheese",
+      price: "$50",
+      url: "./images/pizza_02.jpg",
+      description:
+        "Extra-virgin olive oil, garlic, mozzarella, mushrooms and olives.",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 3,
+      type: "pizza",
+      title: "Apricot Chicken",
+      price: "$60",
+      url: "./images/pizza_03.jpg",
+      description:
+        "Extra-virgin olive oil, garlic, mozzarella, mushrooms and olives.",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 4,
+      type: "pizza",
+      title: "Italiano Original",
+      price: "$50",
+      url: "./images/pizza_04.jpg",
+      description:
+        "Extra-virgin olive oil, garlic, mozzarella, mushrooms and olives.",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 5,
+      type: "pizza",
+      title: "Summer Pizza",
+      price: "$50",
+      url: "./images/pizza_05.jpg",
+      description:
+        "Extra-virgin olive oil, garlic, mozzarella, mushrooms and olives.",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 6,
+      type: "pizza",
+      title: "Chicken Hawaii",
+      price: "$50",
+      url: "./images/pizza_06.jpg",
+      description:
+        "Extra-virgin olive oil, garlic, mozzarella, mushrooms and olives.",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 7,
+      type: "burger",
+      title: "Cheeseburger with Salad",
+      url: "./images/burger_01.jpg",
+      price: "$40",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 8,
+      type: "burger",
+      title: "Royal Cheeseburger with Bacon",
+      price: "$50",
+      url: "./images/burger_02.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 9,
+      type: "burger",
+      title: "Black Gambugrer with Fishcake",
+      price: "$60",
+      url: "./images/burger_03.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 10,
+      type: "burger",
+      title: "Classic Bacon Hamburger",
+      price: "$50",
+      url: "./images/burger_04.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 11,
+      type: "burger",
+      title: "chicken burger",
+      price: "$50",
+      url: "./images/burger_01.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 12,
+      type: "asian food",
+      title: "Salmon Roll",
+      url: "./images/sushi_01.jpg",
+      price: "$40",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 13,
+      type: "asian food",
+      title: "Roll with Tuna",
+      price: "$50",
+      url: "./images/sushi_02.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 14,
+      type: "asian food",
+      title: "Pickled Eel Roll",
+      price: "$60",
+      url: "./images/sushi_03.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 15,
+      type: "asian food",
+      title: "Red Fish Roll",
+      price: "$50",
+      url: "./images/sushi_04.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 16,
+      type: "asian food",
+      title: "Roll With Crab",
+      price: "$50",
+      url: "./images/sushi_05.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+    {
+      id: 17,
+      type: "asian food",
+      title: "Octopus Roll",
+      price: "$50",
+      url: "./images/sushi_07.jpg",
+      description:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+      details:
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    },
+  ];
+  
