da61abc98b06829682e5f4b1c169306c1f7158d3
imansedky5 <imans.ewaiti@gmail.com>
2023-01-01 18:46:47 +0200

    Create food catagory section

---
+++ src/components/FoodCatagories/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./index.css";
+
+type FoodCatagoriesProps = {
+  setCatagoryId: (catagoryId: number) => void;
+};
+
+export const FoodCatagories = ({ setCatagoryId }: FoodCatagoriesProps) => {
+  return (
+    <div className="catagories_wrapper">
+      <button onClick={() => setCatagoryId(1)}>pizza</button>
+      <button onClick={() => setCatagoryId(2)}>asian food</button>
+      <button onClick={() => setCatagoryId(3)}>burger</button>
+    </div>
+  );
+};
